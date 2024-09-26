ddbc961eb5a87225f2830c4d4cf2f5a99e6cc99f
msk102948 <msk102948@gmail.com>
2024-09-26 14:34:52 +0900

    BookmarkType[]을 BookmarkResponse로 변경

---
+++ src/app/components/Bookmarks.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BookmarksResponse } from '@/types';
+import Bookmark from './Bookmark';
+
+type Props = {
+    bookmarks: BookmarksResponse
+}
+
+const Bookmarks: React.FC<Props> = ({bookmarks}) => {
+    return (
+        <div>
+            <h1>Welcome to Bookmarker</h1>
+            {
+            bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+          }            
+        </div>
+    );
+};
+
+export default Bookmarks;
